792a04f4077fdd31d25346c2545cd9775b00a12a
BrianHanifin <>
2019-01-23 08:49:09 -0800

    'git reset --hard upstream/master'; reapplied changes

--- media_player/alexa.py
+++ media_player/alexa.py
@@ -62,8 +62,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_PASSWORD): cv.string,
     vol.Required(CONF_URL): cv.string,
     vol.Optional(CONF_DEBUG, default=False): cv.boolean,
-    vol.Optional(CONF_INCLUDE_DEVICES, default=[]): vol.All(cv.ensure_list, [cv.string]),
-    vol.Optional(CONF_EXCLUDE_DEVICES, default=[]): vol.All(cv.ensure_list, [cv.string]),
+    vol.Optional(CONF_INCLUDE_DEVICES, default=[]):
+        vol.All(cv.ensure_list, [cv.string]),
+    vol.Optional(CONF_EXCLUDE_DEVICES, default=[]):
+        vol.All(cv.ensure_list, [cv.string]),
 })
 
 
@@ -277,7 +279,9 @@ class AlexaClient(MediaPlayerDevice):
         # Class info
         self.alexa_api = AlexaAPI(self, session, url)
         self.auth = authentication
-
+        self.alexa_api_session = session
+        self.alexa_api_url = url
+        
         # Logged in info
         self._authenticated = None
         self._can_access_prime_music = None
@@ -312,6 +316,8 @@ class AlexaClient(MediaPlayerDevice):
         self._source = None
         self._source_list = []
         self.refresh(device)
+        # Last Device
+        self._last_called = None
 
     def _clear_media_details(self):
         """Set all Media Items to None."""
@@ -349,6 +355,7 @@ class AlexaClient(MediaPlayerDevice):
         self._source = self._get_source()
         self._source_list = self._get_source_list()
         session = self.alexa_api.get_state()
+        self._last_called = self._get_last_called()
 
         self._clear_media_details()
         # update the session if it exists; not doing relogin here
@@ -436,11 +443,22 @@ class AlexaClient(MediaPlayerDevice):
             for devices in self._bluetooth_state['pairedDeviceList']:
                 sources.append(devices['friendlyName'])
         return ['Local Speaker'] + sources
+    
+    def _get_last_called(self):
+        last_device_serial = self.alexa_api.get_last_device_serial()
+        if self._device_serial_number == last_device_serial:
+            return True
+        return False
 
     @property
     def available(self):
         """Return the availability of the client."""
         return self._available
+
+    @property
+    def last_device_serial(self):
+        """Return the last activity of the client."""
+        return self._last_device_serial_number
 
     @property
     def unique_id(self):
@@ -615,6 +633,7 @@ class AlexaClient(MediaPlayerDevice):
         """Return the scene state attributes."""
         attr = {
             'available': self._available,
+            'last_called': self._last_called
         }
         return attr
 
@@ -979,6 +998,27 @@ class AlexaAPI():
                 message))
             return None
 
+    def get_last_device_serial(self):
+        """Identify the last device's serial number."""
+        try:
+            response = self._get_request('/api/activities?startTime=&size=1&offset=1')
+        except Exception as ex:
+            template = ("An exception of type {0} occurred."
+                        " Arguments:\n{1!r}")
+            message = template.format(type(ex).__name__, ex.args)
+            _LOGGER.error("An error occured accessing the API: {}".format(
+                message))
+            return None
+        try:
+            response.json()
+        except Exception as ex:
+            template = ("An exception of type {0} occurred."
+                        " Arguments:\n{1!r}")
+            message = template.format(type(ex).__name__, ex.args)
+            _LOGGER.debug("An error occured accessing the API: {}".format(message))
+            return None
+        return response.json()['activities'][0]['sourceDeviceIds'][0]['serialNumber']
+
     def play_music(self, provider_id, search_phrase, customer_id=None):
         """Play Music based on search."""
         data = {
@@ -1126,3 +1166,4 @@ class AlexaAPI():
             _LOGGER.error("An error occured accessing the API: {}".format(
                 message))
             return None
+            

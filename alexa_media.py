07ea5d615d51b52b22fd2bcaba13e9a2b93c44c4
Alan D. Tse <alandtse@gmail.com>
2019-02-02 16:03:07 -0800

    Split API calls to alexapy library

--- alexa_media.py
+++ alexa_media.py
@@ -3,43 +3,27 @@ Support to interface with Alexa Devices.
 
 For more details about this platform, please refer to the documentation at
 https://community.home-assistant.io/t/echo-devices-alexa-as-media-player-testers-needed/58639
-<<<<<<< HEAD:media_player/alexa.py
-VERSION 0.10.2
-=======
 VERSION 1.0.0
->>>>>>> Break up to domain and media_player platform:alexa_media.py
 """
 import logging
 
 from datetime import timedelta
 
-import requests
 import voluptuous as vol
 
 from homeassistant import util
-<<<<<<< HEAD:media_player/alexa.py
-from homeassistant.components.media_player import (
-    MediaPlayerDevice, MEDIA_PLAYER_SCHEMA, PLATFORM_SCHEMA)
-from homeassistant.components.media_player.const import (
-    MEDIA_TYPE_MUSIC, SUPPORT_NEXT_TRACK,
-    SUPPORT_PAUSE, SUPPORT_PLAY, SUPPORT_PREVIOUS_TRACK,
-    SUPPORT_STOP, SUPPORT_TURN_OFF, SUPPORT_VOLUME_MUTE,
-    SUPPORT_PLAY_MEDIA, SUPPORT_VOLUME_SET,
-    DOMAIN,
-    SUPPORT_SELECT_SOURCE)
-=======
->>>>>>> Break up to domain and media_player platform:alexa_media.py
 from homeassistant.const import (
     CONF_EMAIL, CONF_PASSWORD, CONF_URL)
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.event import track_utc_time_change
 from homeassistant.helpers.discovery import load_platform
+from alexapy import AlexaAPI, AlexaLogin
 # from .config_flow import configured_instances
+
+REQUIREMENTS = ['alexapy==0.1.0']
 
 _CONFIGURING = []
 _LOGGER = logging.getLogger(__name__)
-
-REQUIREMENTS = ['beautifulsoup4==4.6.0', 'simplejson==3.16.0']
 
 MIN_TIME_BETWEEN_SCANS = timedelta(seconds=15)
 MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(seconds=1)
@@ -288,372 +272,8 @@ def setup_alexa(hass, config, login_obj):
         configurator = hass.components.configurator
         configurator.async_request_done(config_id)
     _CONFIGURING = []
-<<<<<<< HEAD:media_player/alexa.py
-
-
-class AlexaClient(MediaPlayerDevice):
-    """Representation of a Alexa device."""
-
-    def __init__(self, config, session, device, update_devices, url,
-                 authentication):
-        """Initialize the Alexa device."""
-        # Class info
-        self.alexa_api = AlexaAPI(self, session, url)
-        self.auth = authentication
-        self.alexa_api_session = session
-        self.alexa_api_url = url
-
-        # Logged in info
-        self._authenticated = None
-        self._can_access_prime_music = None
-        self._customer_email = None
-        self._customer_id = None
-        self._customer_name = None
-        self._set_authentication_details(self.auth)
-
-        self.update_devices = update_devices
-        # Device info
-        self._device = None
-        self._device_name = None
-        self._device_serial_number = None
-        self._device_type = None
-        self._device_family = None
-        self._device_owner_customer_id = None
-        self._software_version = None
-        self._available = None
-        self._capabilities = []
-        # Media
-        self._session = None
-        self._media_duration = None
-        self._media_image_url = None
-        self._media_title = None
-        self._media_pos = None
-        self._media_album_name = None
-        self._media_artist = None
-        self._player_state = None
-        self._media_is_muted = None
-        self._media_vol_level = None
-        self._previous_volume = None
-        self._source = None
-        self._source_list = []
-        self.refresh(device)
-        # Last Device
-        self._last_called = None
-
-    def _clear_media_details(self):
-        """Set all Media Items to None."""
-        # General
-        self._media_duration = None
-        self._media_image_url = None
-        self._media_title = None
-        self._media_pos = None
-        self._media_album_name = None
-        self._media_artist = None
-        self._media_player_state = None
-        self._media_is_muted = None
-        self._media_vol_level = None
-
-    def _set_authentication_details(self, auth):
-        """Set Authentication based off auth."""
-        self._authenticated = auth['authenticated']
-        self._can_access_prime_music = auth['canAccessPrimeMusicContent']
-        self._customer_email = auth['customerEmail']
-        self._customer_id = auth['customerId']
-        self._customer_name = auth['customerName']
-
-    def refresh(self, device):
-        """Refresh key device data."""
-        self._device = device
-        self._device_name = device['accountName']
-        self._device_family = device['deviceFamily']
-        self._device_type = device['deviceType']
-        self._device_serial_number = device['serialNumber']
-        self._device_owner_customer_id = device['deviceOwnerCustomerId']
-        self._software_version = device['softwareVersion']
-        self._available = device['online']
-        self._capabilities = device['capabilities']
-        self._bluetooth_state = device['bluetooth_state']
-        self._source = self._get_source()
-        self._source_list = self._get_source_list()
-        session = self.alexa_api.get_state()
-        self._last_called = self._get_last_called()
-
-        self._clear_media_details()
-        # update the session if it exists; not doing relogin here
-        if session is not None:
-            self._session = session
-        if 'playerInfo' in self._session:
-            self._session = self._session['playerInfo']
-            if self._session['state'] is not None:
-                self._media_player_state = self._session['state']
-                self._media_pos = (self._session['progress']['mediaProgress']
-                                   if (self._session['progress'] is not None
-                                       and 'mediaProgress' in
-                                       self._session['progress'])
-                                   else None)
-                self._media_is_muted = (self._session['volume']['muted']
-                                        if (self._session['volume'] is not None
-                                            and 'muted' in
-                                            self._session['volume'])
-                                        else None)
-                self._media_vol_level = (self._session['volume']
-                                                      ['volume'] / 100
-                                         if(self._session['volume'] is not None
-                                             and 'volume' in
-                                             self._session['volume'])
-                                         else None)
-                self._media_title = (self._session['infoText']['title']
-                                     if (self._session['infoText'] is not None
-                                         and 'title' in
-                                         self._session['infoText'])
-                                     else None)
-                self._media_artist = (self._session['infoText']['subText1']
-                                      if (self._session['infoText'] is not None
-                                          and 'subText1' in
-                                          self._session['infoText'])
-                                      else None)
-                self._media_album_name = (self._session['infoText']['subText2']
-                                          if (self._session['infoText'] is not
-                                              None and 'subText2' in
-                                              self._session['infoText'])
-                                          else None)
-                self._media_image_url = (self._session['mainArt']['url']
-                                         if (self._session['mainArt'] is not
-                                             None and 'url' in
-                                             self._session['mainArt'])
-                                         else None)
-                self._media_duration = (self._session['progress']
-                                                     ['mediaLength']
-                                        if (self._session['progress'] is not
-                                            None and 'mediaLength' in
-                                            self._session['progress'])
-                                        else None)
-
-    @property
-    def source(self):
-        """Return the current input source."""
-        return self._source
-
-    @property
-    def source_list(self):
-        """List of available input sources."""
-        return self._source_list
-
-    def select_source(self, source):
-        """Select input source."""
-        if source == 'Local Speaker':
-            self.alexa_api.disconnect_bluetooth()
-            self._source = 'Local Speaker'
-        elif self._bluetooth_state['pairedDeviceList'] is not None:
-            for devices in self._bluetooth_state['pairedDeviceList']:
-                if devices['friendlyName'] == source:
-                    self.alexa_api.set_bluetooth(devices['address'])
-                    self._source = source
-
-    def _get_source(self):
-        source = 'Local Speaker'
-        if self._bluetooth_state['pairedDeviceList'] is not None:
-            for device in self._bluetooth_state['pairedDeviceList']:
-                if device['connected'] is True:
-                    return device['friendlyName']
-        return source
-
-    def _get_source_list(self):
-        sources = []
-        if self._bluetooth_state['pairedDeviceList'] is not None:
-            for devices in self._bluetooth_state['pairedDeviceList']:
-                sources.append(devices['friendlyName'])
-        return ['Local Speaker'] + sources
-
-    def _get_last_called(self):
-        if self._device_serial_number == self.alexa_api.get_last_device_serial():
-            return True
-        return False
-
-    @property
-    def available(self):
-        """Return the availability of the client."""
-        return self._available
-
-    @property
-    def unique_id(self):
-        """Return the id of this Alexa client."""
-        return self.device_serial_number
-
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return self._device_name
-
-    @property
-    def device_serial_number(self):
-        """Return the machine identifier of the device."""
-        return self._device_serial_number
-
-    @property
-    def device(self):
-        """Return the device, if any."""
-        return self._device
-
-    @property
-    def session(self):
-        """Return the session, if any."""
-        return self._session
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        if self._media_player_state == 'PLAYING':
-            return STATE_PLAYING
-        elif self._media_player_state == 'PAUSED':
-            return STATE_PAUSED
-        elif self._media_player_state == 'IDLE':
-            return STATE_IDLE
-        return STATE_STANDBY
-
-    def update(self):
-        """Get the latest details."""
-        self.update_devices(no_throttle=True)
-
-    @property
-    def media_content_type(self):
-        """Return the content type of current playing media."""
-        if self.state in [STATE_PLAYING, STATE_PAUSED]:
-            return MEDIA_TYPE_MUSIC
-        return STATE_STANDBY
-
-    @property
-    def media_artist(self):
-        """Return the artist of current playing media, music track only."""
-        return self._media_artist
-
-    @property
-    def media_album_name(self):
-        """Return the album name of current playing media, music track only."""
-        return self._media_album_name
-
-    @property
-    def media_duration(self):
-        """Return the duration of current playing media in seconds."""
-        return self._media_duration
-
-    @property
-    def media_image_url(self):
-        """Return the image URL of current playing media."""
-        return self._media_image_url
-
-    @property
-    def media_title(self):
-        """Return the title of current playing media."""
-        return self._media_title
-
-    @property
-    def device_family(self):
-        """Return the make of the device (ex. Echo, Other)."""
-        return self._device_family
-
-    @property
-    def supported_features(self):
-        """Flag media player features that are supported."""
-        return SUPPORT_ALEXA
-
-    def set_volume_level(self, volume):
-        """Set volume level, range 0..1."""
-        if not (self.state in [STATE_PLAYING, STATE_PAUSED]
-                and self.available):
-            return
-        self.alexa_api.set_volume(volume)
-        self._media_vol_level = volume
-
-    @property
-    def volume_level(self):
-        """Return the volume level of the client (0..1)."""
-        return self._media_vol_level
-
-    @property
-    def is_volume_muted(self):
-        """Return boolean if volume is currently muted."""
-        if self.volume_level == 0:
-            return True
-        return False
-
-    def mute_volume(self, mute):
-        """Mute the volume.
-
-        Since we can't actually mute, we'll:
-        - On mute, store volume and set volume to 0
-        - On unmute, set volume to previously stored volume
-        """
-        if not (self.state == STATE_PLAYING and self.available):
-            return
-
-        self._media_is_muted = mute
-        if mute:
-            self._previous_volume = self.volume_level
-            self.alexa_api.set_volume(0)
-        else:
-            if self._previous_volume is not None:
-                self.alexa_api.set_volume(self._previous_volume)
-            else:
-                self.alexa_api.set_volume(50)
-
-    def media_play(self):
-        """Send play command."""
-        if not (self.state in [STATE_PLAYING, STATE_PAUSED]
-                and self.available):
-            return
-        self.alexa_api.play()
-
-    def media_pause(self):
-        """Send pause command."""
-        if not (self.state in [STATE_PLAYING, STATE_PAUSED]
-                and self.available):
-            return
-        self.alexa_api.pause()
-
-    def turn_off(self):
-        """Turn the client off."""
-        # Fake it since we can't turn the client off
-        self.media_pause()
-
-    def media_next_track(self):
-        """Send next track command."""
-        if not (self.state in [STATE_PLAYING, STATE_PAUSED]
-                and self.available):
-            return
-        self.alexa_api.next()
-
-    def media_previous_track(self):
-        """Send previous track command."""
-        if not (self.state in [STATE_PLAYING, STATE_PAUSED]
-                and self.available):
-            return
-        self.alexa_api.previous()
-
-    def send_tts(self, message):
-        """Send TTS to Device NOTE: Does not work on WHA Groups."""
-        self.alexa_api.send_tts(message, customer_id=self._customer_id)
-
-    def play_media(self, media_type, media_id, **kwargs):
-        """Send the play_media command to the media player."""
-        if media_type == "music":
-            self.alexa_api.send_tts("Sorry, text to speech can only be called "
-                                    " with the media player alexa tts service")
-        else:
-            self.alexa_api.play_music(media_type, media_id,
-                                      customer_id=self._customer_id)
-
-    @property
-    def device_state_attributes(self):
-        """Return the scene state attributes."""
-        attr = {
-            'available': self._available,
-            'last_called': self._last_called
-        }
-        return attr
-=======
     return True
->>>>>>> Break up to domain and media_player platform:alexa_media.py
+<<<<<<< HEAD
 
 
 class AlexaLogin():
@@ -1194,3 +814,5 @@ class AlexaAPI():
         response = session.get('https://alexa.' + url +
                                '/api/bootstrap')
         return response.json()['authentication']
+=======
+>>>>>>> Split API calls to alexapy library

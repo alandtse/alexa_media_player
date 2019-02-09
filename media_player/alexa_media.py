778feb4bc2a81b97719b34784ed8d1f2a9a6eede
Alan D. Tse <alandtse@gmail.com>
2019-02-08 21:47:08 -0800

    Allow volume setting even if not playing or paused

--- media_player/alexa_media.py
+++ media_player/alexa_media.py
@@ -417,8 +417,7 @@ class AlexaClient(MediaPlayerDevice):
 
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
-        if not (self.state in [STATE_PLAYING, STATE_PAUSED]
-                and self.available):
+        if not self.available:
             return
         self.alexa_api.set_volume(volume)
         self._media_vol_level = volume

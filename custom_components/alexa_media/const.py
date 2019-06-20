a243ca6f72fedd4ab6acf80261db736d5a90af65
Alan D. Tse <alandtse@gmail.com>
2019-06-20 00:41:25 -0700

    Add version info during startup logs

--- custom_components/alexa_media/const.py
+++ custom_components/alexa_media/const.py
@@ -9,6 +9,10 @@ https://community.home-assistant.io/t/echo-devices-alexa-as-media-player-testers
 """
 from datetime import timedelta
 
+__version__ = '1.3.0'
+PROJECT_URL = "https://github.com/keatontaylor/alexa_media_player/"
+ISSUE_URL = "{}issues".format(PROJECT_URL)
+
 DOMAIN = 'alexa_media'
 DATA_ALEXAMEDIA = 'alexa_media'
 
@@ -30,3 +34,13 @@ CONF_EXCLUDE_DEVICES = 'exclude_devices'
 SERVICE_UPDATE_LAST_CALLED = 'update_last_called'
 ATTR_MESSAGE = 'message'
 ATTR_EMAIL = 'email'
+
+STARTUP = """
+-------------------------------------------------------------------
+{}
+Version: {}
+This is a custom component
+If you have any issues with this you need to open an issue here:
+{}
+-------------------------------------------------------------------
+""".format(DOMAIN, __version__, ISSUE_URL)

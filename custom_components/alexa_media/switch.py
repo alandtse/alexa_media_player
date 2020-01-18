16e4b2ca96c7a7a72992ad2780475bd6715398db
Alan D. Tse <alandtse@gmail.com>
2020-01-18 10:26:11 -0800

    fix: enable DND updates on proper websocket

--- custom_components/alexa_media/switch.py
+++ custom_components/alexa_media/switch.py
@@ -270,6 +270,23 @@ class DNDSwitch(AlexaMediaSwitch):
         """Return the icon of the switch."""
         return super()._icon("mdi:do-not-disturb", "mdi:do-not-disturb-off")
 
+    def _handle_event(self, event):
+        """Handle events.
+
+        This will update PUSH_EQUALIZER_STATE_CHANGE events to see if the DND switch
+        should be updated.
+        """
+        try:
+            if not self.enabled:
+                return
+        except AttributeError:
+            pass
+        if "player_state" in event.data:
+            queue_state = event.data["player_state"]
+            if queue_state["dopplerId"]["deviceSerialNumber"] == self._client.unique_id:
+                self._state = getattr(self._client, self._switch_property)
+                self.async_schedule_update_ha_state()
+
 
 class ShuffleSwitch(AlexaMediaSwitch):
     """Representation of a Alexa Media Shuffle switch."""

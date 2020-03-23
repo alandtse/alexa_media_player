9241a075823d8eafe4a58fb22ac14b6f6718bba1
Alan D. Tse <alandtse@gmail.com>
2020-03-23 01:47:41 -0700

    fix: handle recurring reminders fixes #602

--- custom_components/alexa_media/sensor.py
+++ custom_components/alexa_media/sensor.py
@@ -245,19 +245,30 @@ class AlexaMediaNotificationSensor(Entity):
     def _update_recurring_alarm(self, value):
         _LOGGER.debug("value %s", value)
         alarm = value[1][self._sensor_property]
+        reminder = None
+        if isinstance(value[1][self._sensor_property], int):
+            reminder = True
+            alarm = dt.as_local(
+                self._round_time(
+                    datetime.datetime.fromtimestamp(alarm / 1000, tz=LOCAL_TIMEZONE)
+                )
+            )
         alarm_on = value[1]["status"] == "ON"
         recurring_pattern = value[1]["recurringPattern"]
         while (
             alarm_on
             and recurring_pattern
-            and alarm < dt.now()
             and RECURRING_PATTERN_ISO_SET[recurring_pattern]
             and alarm.isoweekday not in RECURRING_PATTERN_ISO_SET[recurring_pattern]
+            and alarm < dt.now()
         ):
             alarm += datetime.timedelta(days=1)
+        if reminder:
+            alarm = dt.as_timestamp(alarm) * 1000
         if alarm != value[1][self._sensor_property]:
             _LOGGER.debug(
-                "Alarm with recurrence %s set to %s",
+                "%s with recurrence %s set to %s",
+                value[1]["type"],
                 RECURRING_PATTERN[recurring_pattern],
                 alarm,
             )

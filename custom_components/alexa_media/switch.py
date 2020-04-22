51f39799729aae20bff896287bf278992e20c325
Alan D. Tse <alandtse@gmail.com>
2020-04-21 19:35:43 -0700

    style: remove unnecessary parantheses

--- custom_components/alexa_media/switch.py
+++ custom_components/alexa_media/switch.py
@@ -54,11 +54,9 @@ async def async_setup_platform(hass, config, add_devices_callback, discovery_inf
         if key not in (
             hass.data[DATA_ALEXAMEDIA]["accounts"][account]["entities"]["switch"]
         ):
-            (
-                hass.data[DATA_ALEXAMEDIA]["accounts"][account]["entities"]["switch"][
-                    key
-                ]
-            ) = {}
+            hass.data[DATA_ALEXAMEDIA]["accounts"][account]["entities"]["switch"][
+                key
+            ] = {}
             for (switch_key, class_) in SWITCH_TYPES:
                 if (
                     switch_key == "dnd"

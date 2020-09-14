f00a50c25ffd8796aa59401ff68bae784b5d366c
semantic-release <semantic-release>
2020-09-14 01:40:00 +0000

    3.0.0
    Automatically generated by python-semantic-release

--- custom_components/alexa_media/const.py
+++ custom_components/alexa_media/const.py
@@ -9,7 +9,7 @@ https://community.home-assistant.io/t/echo-devices-alexa-as-media-player-testers
 """
 from datetime import timedelta
 
-__version__ = "2.11.2"
+__version__ = "3.0.0"
 PROJECT_URL = "https://github.com/custom-components/alexa_media_player/"
 ISSUE_URL = "{}issues".format(PROJECT_URL)
 

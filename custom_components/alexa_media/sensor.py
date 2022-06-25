33f5d4c2b70c976641ff419d9d791ed0970b99d7
Alan D. Tse <alandtse@gmail.com>
2022-06-25 14:31:57 -0700

    refactor: add next_alarm label

--- custom_components/alexa_media/sensor.py
+++ custom_components/alexa_media/sensor.py
@@ -228,7 +228,7 @@ class AlexaMediaNotificationSensor(Entity):
         self,
         client,
         n_dict,
-        sensor_property: Text,
+        sensor_property: str,
         account,
         name="Next Notification",
         icon=None,
@@ -252,9 +252,9 @@ class AlexaMediaNotificationSensor(Entity):
         self._tracker: Optional[Callable] = None
         self._state: Optional[datetime.datetime] = None
         self._dismissed: Optional[datetime.datetime] = None
-        self._status: Optional[Text] = None
-        self._amz_id: Optional[Text] = None
-        self._version: Optional[Text] = None
+        self._status: Optional[str] = None
+        self._amz_id: Optional[str] = None
+        self._version: Optional[str] = None
 
     def _process_raw_notifications(self):
         self._all = (
@@ -356,17 +356,18 @@ class AlexaMediaNotificationSensor(Entity):
 
     def _update_recurring_alarm(self, value):
         _LOGGER.debug("Sensor value %s", value)
-        alarm = value[1][self._sensor_property]
+        next_item = value[1]
+        alarm = next_item[self._sensor_property]
         reminder = None
-        if isinstance(value[1][self._sensor_property], (int, float)):
+        if isinstance(next_item[self._sensor_property], (int, float)):
             reminder = True
             alarm = dt.as_local(
                 self._round_time(
                     datetime.datetime.fromtimestamp(alarm / 1000, tz=LOCAL_TIMEZONE)
                 )
             )
-        alarm_on = value[1]["status"] == "ON"
-        recurring_pattern = value[1].get("recurringPattern")
+        alarm_on = next_item["status"] == "ON"
+        recurring_pattern = next_item.get("recurringPattern")
         while (
             alarm_on
             and recurring_pattern
@@ -377,14 +378,14 @@ class AlexaMediaNotificationSensor(Entity):
             alarm += datetime.timedelta(days=1)
         if reminder:
             alarm = dt.as_timestamp(alarm) * 1000
-        if alarm != value[1][self._sensor_property]:
+        if alarm != next_item[self._sensor_property]:
             _LOGGER.debug(
                 "%s with recurrence %s set to %s",
-                value[1]["type"],
+                next_item["type"],
                 RECURRING_PATTERN.get(recurring_pattern),
                 alarm,
             )
-            value[1][self._sensor_property] = alarm
+            next_item[self._sensor_property] = alarm
         return value
 
     @staticmethod

ace422721d5e9c5d19a8fb919f0e60ebd7303d2a
Felix Häberle <34959078+felixhaeberle@users.noreply.github.com>
2023-05-04 11:42:51 +0000

    update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -1,9 +1,9 @@
 export async function defineConfig(env) {
-  const {
-    default: pluginJson
-  } = await env.$import(
-    'https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js'
-  );
+    const {
+      default: jsonPlugin
+    } = await env.$import(
+      "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@2/dist/index.js",
+    )
 
   const {
     default: standardLintRules
@@ -14,7 +14,7 @@ export async function defineConfig(env) {
   return {
     referenceLanguage: 'en',
     plugins: [
-      pluginJson({
+      jsonPlugin({
         pathPattern: './custom_components/translations/{language}.json'
       }),
       standardLintRules()

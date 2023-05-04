3249d552282526d79fbbc9db2b799880f7ec71ba
Felix Häberle <34959078+felixhaeberle@users.noreply.github.com>
2023-05-04 11:34:55 +0000

    update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -2,7 +2,7 @@ export async function defineConfig(env) {
   const {
     default: pluginJson
   } = await env.$import(
-    'https://cdn.jsdelivr.net/npm/@samuelstroschein/inlang-plugin-json@latest/dist/index.js'
+    'https://cdn.jsdelivr.net/gh/@samuelstroschein/inlang-plugin-json@latest/dist/index.js'
   );
 
   const {

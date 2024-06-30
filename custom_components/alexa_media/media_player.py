7ea65fcaf47867fd00a8a2f811a4ccc4e716a23b
Petro31 <35082313+Petro31@users.noreply.github.com>
2024-06-30 02:24:54 -0400

    fix: fix warning for async_forward_entry_setup (#2294)

--- custom_components/alexa_media/media_player.py
+++ custom_components/alexa_media/media_player.py
@@ -187,8 +187,8 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
                 )
                 config_entry.async_create_task(
                     hass,
-                    hass.config_entries.async_forward_entry_setup(
-                        config_entry, component
+                    hass.config_entries.async_forward_entry_setups(
+                        config_entry, [component]
                     )
                 )
         return True

01b969935cac2bbe9fb7442404f1d4844874bb33
Daniel <49846893+danielbrunt57@users.noreply.github.com>
2024-11-16 22:33:52 -0800

    revert: "fix: missing public_url (#2670)" (#2684)
    This reverts commit bf6fd10ff0287bef4923694672554587a69e9f1f.

--- custom_components/alexa_media/__init__.py
+++ custom_components/alexa_media/__init__.py
@@ -153,10 +153,9 @@ async def async_setup(hass, config, discovery_info=None):
                     hass.config_entries.async_update_entry(
                         entry,
                         data={
-                            CONF_URL: account[CONF_URL],
                             CONF_EMAIL: account[CONF_EMAIL],
                             CONF_PASSWORD: account[CONF_PASSWORD],
-                            CONF_PUBLIC_URL: account[CONF_PUBLIC_URL],
+                            CONF_URL: account[CONF_URL],
                             CONF_INCLUDE_DEVICES: account[CONF_INCLUDE_DEVICES],
                             CONF_EXCLUDE_DEVICES: account[CONF_EXCLUDE_DEVICES],
                             CONF_SCAN_INTERVAL: account[
@@ -187,7 +186,7 @@ async def async_setup(hass, config, discovery_info=None):
                         CONF_URL: account[CONF_URL],
                         CONF_EMAIL: account[CONF_EMAIL],
                         CONF_PASSWORD: account[CONF_PASSWORD],
-                        CONF_PUBLIC_URL: account.get(CONF_PUBLIC_URL, ""),
+                        CONF_PUBLIC_URL: account[CONF_PUBLIC_URL],
                         CONF_INCLUDE_DEVICES: account[CONF_INCLUDE_DEVICES],
                         CONF_EXCLUDE_DEVICES: account[CONF_EXCLUDE_DEVICES],
                         CONF_SCAN_INTERVAL: account[CONF_SCAN_INTERVAL].total_seconds(),

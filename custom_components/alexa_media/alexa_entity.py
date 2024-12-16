5756f48d110f9298d5f866f44a0683e0d4a69f05
alams154 <31905246+alams154@users.noreply.github.com>
2024-12-16 13:23:16 -0600

    fix: Move variable assignment out of condition for manufacturer check

--- custom_components/alexa_media/alexa_entity.py
+++ custom_components/alexa_media/alexa_entity.py
@@ -87,10 +87,8 @@ def is_local(appliance: dict[str, Any]) -> bool:
     # Ledvance/Sengled bulbs connected via bluetooth are hard to detect as locally connected
     # Amazon devices are not local but bypassing the local check allows for control by the integration
     # There is probably a better way, but this works for now.
-    if (
-        manufacturerNames = ["Ledvance", "Sengled", "Amazon"]
-        appliance.get("manufacturerName") in manufacturerNames
-    ):
+    manufacturerNames = ["Ledvance", "Sengled", "Amazon"]
+    if (appliance.get("manufacturerName") in manufacturerNames):
         return not is_skill(appliance)
 
     # Zigbee devices are guaranteed to be local and have a particular pattern of id

98d403b9c57350fd1f7e3f536dd0834945076dca
Daniel <daniel@brunt.ca>
2026-02-07 05:20:39 -0800

    Remove unused import (Callable)

--- custom_components/alexa_media/light.py
+++ custom_components/alexa_media/light.py
@@ -7,7 +7,6 @@ For more details about this platform, please refer to the documentation at
 https://community.home-assistant.io/t/echo-devices-alexa-as-media-player-testers-needed/58639
 """
 
-from collections.abc import Callable
 import datetime
 import logging
 from math import sqrt

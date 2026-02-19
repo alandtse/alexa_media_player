e78eab0b15b3bcc1814967f55228acdd21583cd3
Daniel <daniel@brunt.ca>
2026-02-19 08:04:53 -0800

    Update __init__.py

--- custom_components/alexa_media/__init__.py
+++ custom_components/alexa_media/__init__.py
@@ -7,7 +7,7 @@ For more details about this platform, please refer to the documentation at
 https://community.home-assistant.io/t/echo-devices-alexa-as-media-player-testers-needed/58639
 """
 
-from contextlib import suppress
+import asyncio
 from datetime import datetime, timedelta
 from json import JSONDecodeError, loads
 import logging

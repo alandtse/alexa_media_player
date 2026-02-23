5b07aeba653da2ae4fe5d30782a78a82f6ebc877
Daniel <daniel@brunt.ca>
2026-02-22 19:34:21 -0800

    Line 1198 - “Task cancelled during unload/shutdown; propagate cancellation.”, `raise`

--- custom_components/alexa_media/__init__.py
+++ custom_components/alexa_media/__init__.py
@@ -1195,7 +1195,8 @@ async def setup_alexa(hass, config_entry, login_obj: AlexaLogin):
                 async with async_timeout.timeout(10):
                     last_called = await AlexaAPI.get_last_device_serial(login_obj)
             except asyncio.CancelledError:
-                return
+                “Task cancelled during unload/shutdown; propagate cancellation.”
+                raise
             except TypeError:
                 _LOGGER.debug(
                     "%s: Error updating last_called: %s",
